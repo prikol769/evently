3827095a04e91c14d31c5543e0b1a1519bd28bb9
prikol769 <prikol769@gmail.com>
2024-01-14 18:16:39 -0600

    added MobileNav component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,9 @@
+import {SignedIn, SignedOut, UserButton} from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import {Button} from "../ui/button"
+import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
@@ -14,8 +18,24 @@ const Header = () => {
           />
         </Link>
 
-        <div className="flex w-32 justify-end gap-3">
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
 
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
         </div>
       </div>
     </header>
